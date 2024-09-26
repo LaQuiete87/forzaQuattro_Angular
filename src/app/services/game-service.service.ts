87b513b852224a702a0eb430e162cf822d05dd56
Daigoro <daigoro87@hotmail.it>
2024-09-26 20:21:15 +0200

    implementato nel service: - i metodi per controllare il forza 4 in tutte le direzioni. - i metodi per cercare un trio sensato in tutte le direzioni. -metodo per cercare una coppia sensata in orizzontale

--- src/app/services/game-service.service.ts
+++ src/app/services/game-service.service.ts
@@ -49,4 +49,396 @@ export class GameServiceService {
   //     return index
   //   });
   // }
+
+  //CERCA COMBINAZIONI DI QUATERNE IN ORIZZONTALE \ VERTICALE\ OBLIQUO DESTO E SINISTRO
+  forza4Horizontal(
+    numRow: number,
+    numCol: number,
+    grid: string[][],
+    currentPlayer: string
+  ): boolean {
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 4;
+        indiceColonna++
+      ) {
+        //Controllo per CPU-1
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 3] === currentPlayer
+        ) {
+          console.log(`${currentPlayer} ha fatto streak diagonale destro`);
+          // this.winner = true;
+          // this.typeOfStreak = 'diagonale destro';
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  forza4Vertical(
+    numRow: number,
+    numCol: number,
+    grid: string[][],
+    currentPlayer: string
+  ): boolean {
+    //streak verticale
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 1;
+        indiceColonna++
+      ) {
+        //Controllo per CPU-1
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna] === currentPlayer
+        ) {
+          console.log('CPU_1 Ha fatto streak verticale ');
+          // this.winner = true;
+          // this.typeOfStreak = 'verticale';
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  forza4Diagonal(
+    numRow: number,
+    numCol: number,
+    grid: string[][],
+    currentPlayer: string
+  ): boolean {
+    //streak diagonale destro
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 4;
+        indiceColonna++
+      ) {
+        
+        if (
+          grid[indiceRiga][numCol] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna + 3] === currentPlayer
+        ) {
+          console.log('CPU_1 Ha fatto streak diagonale destro ');
+          // this.winner = true;
+          // this.typeOfStreak = 'diagonale destro';
+          return true;
+        }
+      }
+    }
+    //streak diagonale sinistro
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = numCol - 1;
+        indiceColonna >= 3;
+        indiceColonna--
+      ) {
+        //Controllo per CPU-1
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna - 1] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna - 2] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna - 3] === currentPlayer
+        ) {
+          console.log('CPU_1 Ha fatto streak diagonale sinistro ');
+          // this.winner = true;
+          // this.typeOfStreak = 'diagonale sinistro';
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  //CERCA TRIO SENSATO ORIZZONTALE \ VERTICALE\ OBLIQUO DESTRO E SINISTRO
+
+  //Cerca un trio sensato orizzontale e restituisce le coordinate della cella da occupare (obbligatoriamente perché porta ad una vincita/blocco certi)
+  findTrioeHorizontal(numRow: number,numCol: number,grid: string[][],currentPlayer: string) {
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 4;
+        indiceColonna++
+      ) {
+        //Controllo combinazione XXXnull
+        if (grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 3] === null &&
+          grid[indiceRiga+1][indiceColonna + 3] !=null) {
+         
+          console.log("Trio orizzontale sensato")
+          return grid[indiceRiga][indiceColonna + 3];
+        }
+            //Controllo combinazione XXnullX
+            if (grid[indiceRiga][indiceColonna] === currentPlayer &&
+              grid[indiceRiga][indiceColonna + 1] === currentPlayer &&
+              grid[indiceRiga][indiceColonna + 2] === null &&
+              grid[indiceRiga][indiceColonna + 3] === currentPlayer &&
+              grid[indiceRiga+1][indiceColonna + 2] !=null) {
+             
+                console.log("Trio orizzontale sensato")
+                return grid[indiceRiga][indiceColonna + 2]
+            }
+        //Controllo combinazione XnullXX
+        if (grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 1] === null &&
+          grid[indiceRiga][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 3] === currentPlayer &&
+          grid[indiceRiga+1][indiceColonna + 1] !=null) {
+         
+            console.log("Trio orizzontale sensato")
+            return grid[indiceRiga][indiceColonna + 1]
+        }
+
+         //Controllo combinazione nullXXX
+         if (grid[indiceRiga][indiceColonna] === null &&
+          grid[indiceRiga][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 3] === currentPlayer &&
+          grid[indiceRiga+1][indiceColonna] !=null) {
+         
+            console.log("Trio orizzontale sensato")
+            return grid[indiceRiga][indiceColonna]
+        }
+
+      
+      }
+    }
+    return false;
+  }
+  findTrioeDiagonal(numRow: number, numCol: number, grid: string[][],currentPlayer: string) {
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+       //trio diagonale destro
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 4;
+        indiceColonna++
+      ) {
+        //Controllo combinazione XXXnull
+        if (
+          grid[indiceRiga][numCol] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna + 3] === null &&
+          grid[indiceRiga-2][indiceColonna + 3] !=null
+        ) {
+          console.log('Trio diagonale sensato ');
+       
+          return  grid[indiceRiga - 3][indiceColonna + 3];
+        }
+
+             //Controllo combinazione XXnullX
+             if (
+              grid[indiceRiga][numCol] === currentPlayer &&
+              grid[indiceRiga - 1][indiceColonna + 1] === currentPlayer &&
+              grid[indiceRiga - 2][indiceColonna + 2] === null &&
+              grid[indiceRiga - 3][indiceColonna + 3] === currentPlayer &&
+              grid[indiceRiga - 1][indiceColonna + 2] !=null
+            ) {
+              console.log('Trio diagonale sensato ');
+           
+              return grid[indiceRiga - 2][indiceColonna + 2];
+            }
+
+            //Controllo combinazione XnullXX
+            if (
+              grid[indiceRiga][numCol] === currentPlayer &&
+              grid[indiceRiga - 1][indiceColonna + 1] === null &&
+              grid[indiceRiga - 2][indiceColonna + 2] === currentPlayer &&
+              grid[indiceRiga - 3][indiceColonna + 3] === currentPlayer &&
+              grid[indiceRiga][indiceColonna + 1] !=null
+            ) {
+              console.log('Trio diagonale sensato ');
+           
+              return grid[indiceRiga - 1][indiceColonna + 1];
+            }
+
+             //Controllo combinazione nullXXX
+             if (
+              grid[indiceRiga][numCol] === null &&
+              grid[indiceRiga - 1][indiceColonna + 1] === currentPlayer &&
+              grid[indiceRiga - 2][indiceColonna + 2] === currentPlayer &&
+              grid[indiceRiga - 3][indiceColonna + 3] === currentPlayer &&
+              grid[indiceRiga+1]?.[indiceColonna] !=null
+            ) {
+              console.log('Trio diagonale sensato ');
+           
+              return grid[indiceRiga][numCol];
+            }
+      }
+    }
+    //trio diagonale sinistro
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = numCol - 1;
+        indiceColonna >= 3;
+        indiceColonna--
+      ) {
+       //Controllo combinazione XXXnull
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna - 1] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna - 2] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna - 3] === null &&
+          grid[indiceRiga-2][indiceColonna - 3] !=null
+        ) {
+          console.log('Trio diagonale sensato ');
+          return  grid[indiceRiga - 3][indiceColonna - 3];
+        }
+
+        //Controllo combinazione XXnullX
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna - 1] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna - 2] === null &&
+          grid[indiceRiga - 3][indiceColonna - 3] === currentPlayer &&
+          grid[indiceRiga-1][indiceColonna - 2] !=null
+        ) {
+          console.log('Trio diagonale sensato ');
+          return   grid[indiceRiga - 2][indiceColonna - 2];
+        }
+
+        //Controllo combinazione XnullXX
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna - 1] === null &&
+          grid[indiceRiga - 2][indiceColonna - 2] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna - 3] === currentPlayer &&
+          grid[indiceRiga][indiceColonna - 1] !=null
+        ) {
+          console.log('Trio diagonale sensato ');
+          return  grid[indiceRiga - 1][indiceColonna - 1];
+        }
+
+        //Controllo combinazione nullXXX
+        if (
+          grid[indiceRiga][indiceColonna] === null &&
+          grid[indiceRiga - 1][indiceColonna - 1] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna - 2] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna - 3] === currentPlayer &&
+          grid[indiceRiga+1]?.[indiceColonna] !=null  
+        ) {
+          console.log('Trio diagonale sensato ');
+          return  grid[indiceRiga][indiceColonna];
+        }
+      }
+    }
+
+    return false;
+  }
+
+  findTrioeVertical(numRow: number, numCol: number, grid: string[][], currentPlayer: string) {
+     //streak verticale
+     for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 1;
+        indiceColonna++
+      ) {
+        //Controllo combinazione XXXnull
+        if (
+          grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 1][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 2][indiceColonna] === currentPlayer &&
+          grid[indiceRiga - 3][indiceColonna] === null 
+        ) {
+          console.log('Trio verticale sensato ');
+          return grid[indiceRiga - 3][indiceColonna];
+        }
+      }
+    }
+    return false;
+  }
+
+  //CERCA COPPIA SENSATA ORIZZONTALE \ VERTICALE\ OBLIQUO DESTRO E SINISTRO
+  findCoupleHorizontal(numRow: number, numCol: number, grid: string[][], currentPlayer: string) {
+    for (let indiceRiga = numRow - 1; indiceRiga >= 3; indiceRiga--) {
+      for (
+        let indiceColonna = 0;
+        indiceColonna <= numCol - 4;
+        indiceColonna++
+      ) {
+        //Controllo combinazione XXnullnull
+        if (grid[indiceRiga][indiceColonna] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 2] === null &&
+          grid[indiceRiga][indiceColonna + 3] === null) {
+            if(grid[indiceRiga+1]?.[indiceColonna + 2] !== null){
+              console.log("Coppia orizzontale sensata")
+              return grid[indiceRiga][indiceColonna + 2]
+            }
+            if(grid[indiceRiga+1]?.[indiceColonna + 3] !== null){
+              console.log("Coppia orizzontale sensata")
+              return grid[indiceRiga][indiceColonna + 3]
+            }
+         
+         
+         
+        }
+          //Controllo combinazione XnullnullX
+          if (grid[indiceRiga][indiceColonna] === currentPlayer &&
+            grid[indiceRiga][indiceColonna + 1] === null &&
+            grid[indiceRiga][indiceColonna + 2] === null &&
+            grid[indiceRiga][indiceColonna + 3] === currentPlayer) {
+              if(grid[indiceRiga+1]?.[indiceColonna + 1] !== null){
+                console.log("Coppia orizzontale sensata")
+                return grid[indiceRiga][indiceColonna + 1]
+              }
+              if(grid[indiceRiga+1]?.[indiceColonna + 2] !== null){
+                console.log("Coppia orizzontale sensata")
+                return grid[indiceRiga][indiceColonna + 2]
+              }      
+          }
+           
+        //Controllo combinazione nullnullXX
+        if (grid[indiceRiga][indiceColonna] === null &&
+          grid[indiceRiga][indiceColonna + 1] === null &&
+          grid[indiceRiga][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 3] === currentPlayer) {
+            if(grid[indiceRiga+1]?.[indiceColonna] !== null){
+              console.log("Coppia orizzontale sensata")
+              return grid[indiceRiga][indiceColonna]
+            }
+            if(grid[indiceRiga+1]?.[indiceColonna + 1] !== null){
+              console.log("Coppia orizzontale sensata")
+              return grid[indiceRiga][indiceColonna + 1]
+            }      
+        }
+  
+         //Controllo combinazione nullXXnull
+         if (grid[indiceRiga][indiceColonna] === null &&
+          grid[indiceRiga][indiceColonna + 1] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 2] === currentPlayer &&
+          grid[indiceRiga][indiceColonna + 3] === null) {
+            if(grid[indiceRiga+1]?.[indiceColonna] !== null){
+              console.log("Coppia orizzontale sensata")
+              return grid[indiceRiga][indiceColonna]
+            }
+            if(grid[indiceRiga+1]?.[indiceColonna + 3] !== null){
+              console.log("Coppia orizzontale sensata")
+              return grid[indiceRiga][indiceColonna + 3]
+            }      
+        }
+      
+      }
+    }
+    return false;
+  }
+ 
+
 }
+
+
